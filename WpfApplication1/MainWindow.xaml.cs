7526aa183f191281d9e8107d29ffb6c5a6a9491f
Chris House <chris@chrishouse.us>
2016-05-20 08:24:48 -0500

    Adding initial WPF form. This form contains the RH theme I will be using. Still need to be abstracted away in the correct place.

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Shell;
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            TaskbarItemInfo taskbarItemInfo = new TaskbarItemInfo();
+            taskbarItemInfo.ProgressValue = .5;
+            taskbarItemInfo.ProgressState = TaskbarItemProgressState.Normal;
+        }
+
+        private void Speak(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+            TaskbarItemInfo taskbarItemInfo = new TaskbarItemInfo();
+            taskbarItemInfo.ProgressValue = .5;
+            taskbarItemInfo.ProgressState = TaskbarItemProgressState.Paused;
+
+
+
+
+        }
+        private void PART_CLOSE_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void PART_MINIMIZE_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Minimized;
+        }
+        private void PART_MAXIMIZE_RESTORE_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == System.Windows.WindowState.Normal)
+            {
+                this.WindowState = System.Windows.WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = System.Windows.WindowState.Normal;
+            }
+        }
+        private void PART_TITLEBAR_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
